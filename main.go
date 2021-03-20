13cb548c262c4580310a8e6099beb10060f00e2d
asliusar <andrii.sliusar@gmail.com>
2021-03-21 00:34:11 +0100

    fixed email subjects

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ func NewGmailClient(email, password string) *GmailClient {
 }
 
 func (client GmailClient) SendEmail(to []string, data *GmailSendData) (bool, error) {
-	subject := data.Subject
+	subject := fmt.Sprintf("Subject: %s\n", data.Subject)
 	mime := "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n\n"
 	msg := []byte(subject + mime + data.Body)
 
